a4bf015e7b150789ae31e5c704c4b64b6e5518b7
ankish08 <55591041+ankish08@users.noreply.github.com>
2024-09-17 16:44:30 +0100

    Update Alerts.cy.js

--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -5,8 +5,6 @@ describe('Alerts', () =>{
 
     it('Authenticated Alert', ()=> {
 
-        cy.visit('http://admin:admin@the-internet.herokuapp.com/basic_auth')
-        
-        cy.get("div[class='example'] h3").should('have.contain','Basic')
+    
     })
 })
